2a6b2f2ce0583577b1b7624e564f9f8efc679562
MiheyM2019 <mm2019@ic.ac.uk>
2022-06-08 01:32:39 +0100

    can now have custom strings in box

--- quacker/hintsdisplay.cpp
+++ quacker/hintsdisplay.cpp
@@ -40,26 +40,38 @@ void HintsDisplay::positionChanged(const Quackle::GamePosition &position)
   showHints("Hello, world!");
 }
 
-void HintsDisplay::showHints(const Quackle::LongLetterString &tiles) {
+void HintsDisplay::showHints(const Quackle::LongLetterString &hints) {
+  if (hints.empty())
+  {
+    m_label->setText(tr("No hints to give."));
+    return;
+  }
+
   QFontMetrics metrics(m_textEdit->currentFont());
   int maxLineWidth = 0;
 
-  QString line = "AAAAA AAAAA AAAAA";
+  QString text;
+
+  text.append(QString::fromStdString(hints));
+
   #if (QT_VERSION >= QT_VERSION_CHECK(5, 11, 0))
     // Kill Qt 5.13 deprecation warning without breaking pre-5.11 builds
-    const int lineWidth = metrics.horizontalAdvance(line);
+    const int lineWidth = metrics.horizontalAdvance(text);
   #else
-    const int lineWidth = metrics.width(line);
+    const int lineWidth = metrics.width(text);
   #endif
 
   if (lineWidth > maxLineWidth)
     maxLineWidth = lineWidth;
 
-  const int minimumMaxLineWidth = 16;
+  m_label->setText("Your hint:");
+  m_textEdit->setPlainText(text);
+
+  const int minimumMaxLineWidth = 32;
   if (maxLineWidth < minimumMaxLineWidth)
     maxLineWidth = minimumMaxLineWidth;
 
-  const int maximumWidth = 3 * (maxLineWidth + m_textEdit->frameWidth() * 2 + (m_textEdit->verticalScrollBar()->isVisible()? m_textEdit->verticalScrollBar()->width() : 0) + 10) / 2;
+  const int maximumWidth = maxLineWidth + m_textEdit->frameWidth() * 2 + (m_textEdit->verticalScrollBar()->isVisible()? m_textEdit->verticalScrollBar()->width() : 0) + 10;
   m_textEdit->setMaximumSize(maximumWidth, 26 * 100);
   m_textEdit->resize(m_textEdit->maximumSize());
 }
