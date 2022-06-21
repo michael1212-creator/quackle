6e3251be4fb684829bf6904f01a6bc862463602c
MiheyM2019 <mm2019@ic.ac.uk>
2022-06-21 08:36:02 +0100

    formatting

--- evaluator.cpp
+++ evaluator.cpp
@@ -16,150 +16,152 @@
  *  along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include "evaluator.h"
 #include "board.h"
+#include "catchall.h"
 #include "datamanager.h"
 #include "game.h"
-#include "evaluator.h"
 #include "strategyparameters.h"
-#include "catchall.h"
 
 using namespace Quackle;
 
-double Evaluator::equity(const GamePosition &position, const Move &move) const
-{
-	(void) position;
-	return move.effectiveScore();
+double Evaluator::equity(const GamePosition &position, const Move &move) const {
+  (void)position;
+  return move.effectiveScore();
 }
 
-double Evaluator::playerConsideration(const GamePosition &position, const Move &move) const
-{
-	(void) position;
-	(void) move;
-	return 0;
+double Evaluator::playerConsideration(const GamePosition &position,
+                                      const Move &move) const {
+  (void)position;
+  (void)move;
+  return 0;
 }
 
-double Evaluator::sharedConsideration(const GamePosition &position, const Move &move) const
-{
-	(void) position;
-	(void) move;
-	return 0;
+double Evaluator::sharedConsideration(const GamePosition &position,
+                                      const Move &move) const {
+  (void)position;
+  (void)move;
+  return 0;
 }
 
-double Evaluator::leaveValue(const LetterString &leave) const
-{
-	(void) leave;
-	return 0;
+double Evaluator::leaveValue(const LetterString &leave) const {
+  (void)leave;
+  return 0;
 }
 
 ////////////
 
-double ScorePlusLeaveEvaluator::equity(const GamePosition &position, const Move &move) const
-{
-	return playerConsideration(position, move) + sharedConsideration(position, move) + move.effectiveScore();
+double ScorePlusLeaveEvaluator::equity(const GamePosition &position,
+                                       const Move &move) const {
+  return playerConsideration(position, move) +
+         sharedConsideration(position, move) + move.effectiveScore();
 }
 
-double ScorePlusLeaveEvaluator::playerConsideration(const GamePosition &position, const Move &move) const
-{
-	return leaveValue((position.currentPlayer().rack() - move).tiles());
+double
+ScorePlusLeaveEvaluator::playerConsideration(const GamePosition &position,
+                                             const Move &move) const {
+  return leaveValue((position.currentPlayer().rack() - move).tiles());
 }
 
-double ScorePlusLeaveEvaluator::sharedConsideration(const GamePosition &position, const Move &move) const
-{
-	(void) position;
-	(void) move;
-	return 0;
+double
+ScorePlusLeaveEvaluator::sharedConsideration(const GamePosition &position,
+                                             const Move &move) const {
+  (void)position;
+  (void)move;
+  return 0;
 }
 
-double ScorePlusLeaveEvaluator::leaveValue(const LetterString &leave) const
-{
-	LetterString alphabetized = String::alphabetize(leave);
-	
-	if (QUACKLE_STRATEGY_PARAMETERS->hasSuperleaves() && QUACKLE_STRATEGY_PARAMETERS->superleave(alphabetized))
-		return QUACKLE_STRATEGY_PARAMETERS->superleave(alphabetized);
+double ScorePlusLeaveEvaluator::leaveValue(const LetterString &leave) const {
+  LetterString alphabetized = String::alphabetize(leave);
 
-	double value = 0;
+  if (QUACKLE_STRATEGY_PARAMETERS->hasSuperleaves() &&
+      QUACKLE_STRATEGY_PARAMETERS->superleave(alphabetized))
+    return QUACKLE_STRATEGY_PARAMETERS->superleave(alphabetized);
 
-	if (!leave.empty())
-	{
-		double synergy = 0;
-		LetterString uniqleave;
+  double value = 0;
 
-		if (QUACKLE_STRATEGY_PARAMETERS->hasWorths())
-			for (const auto& leaveIt : leave)
-				value += QUACKLE_STRATEGY_PARAMETERS->tileWorth(leaveIt);
+  if (!leave.empty()) {
+    double synergy = 0;
+    LetterString uniqleave;
 
-		if (QUACKLE_STRATEGY_PARAMETERS->hasSyn2())
-			for (unsigned int i = 0; i < alphabetized.length() - 1; ++i)
-				if (alphabetized[i] == alphabetized[i + 1])
-					value += QUACKLE_STRATEGY_PARAMETERS->syn2(alphabetized[i], alphabetized[i]);
+    if (QUACKLE_STRATEGY_PARAMETERS->hasWorths())
+      for (const auto &leaveIt : leave)
+        value += QUACKLE_STRATEGY_PARAMETERS->tileWorth(leaveIt);
 
-		uniqleave += alphabetized[0];
-		for (unsigned int i = 1; i < alphabetized.length(); ++i)
-			if (uniqleave[uniqleave.length() - 1] != alphabetized[i])
-				uniqleave += alphabetized[i];
+    if (QUACKLE_STRATEGY_PARAMETERS->hasSyn2())
+      for (unsigned int i = 0; i < alphabetized.length() - 1; ++i)
+        if (alphabetized[i] == alphabetized[i + 1])
+          value += QUACKLE_STRATEGY_PARAMETERS->syn2(alphabetized[i],
+                                                     alphabetized[i]);
 
-		if (uniqleave.length() >= 2 && QUACKLE_STRATEGY_PARAMETERS->hasSyn2())
-		{
-			for (unsigned int i = 0; i < uniqleave.length() - 1; ++i)
-				for (unsigned int j = i + 1; j < uniqleave.length(); ++j)
-					synergy += QUACKLE_STRATEGY_PARAMETERS->syn2(uniqleave[i], uniqleave[j]);
+    uniqleave += alphabetized[0];
+    for (unsigned int i = 1; i < alphabetized.length(); ++i)
+      if (uniqleave[uniqleave.length() - 1] != alphabetized[i])
+        uniqleave += alphabetized[i];
 
-			// TODO handle the Q
-			
-			bool holding_bad_tile = false;
-			for (unsigned int i = 0; i < uniqleave.length(); ++i) {
-				if (QUACKLE_STRATEGY_PARAMETERS->tileWorth(uniqleave[i]) < -5.5) {
-					holding_bad_tile = true;
-				}
-			}
-			
-			if ((synergy > 3.0) && !holding_bad_tile) {
-			    synergy += 1.5 * (synergy - 3.0);
-			}
+    if (uniqleave.length() >= 2 && QUACKLE_STRATEGY_PARAMETERS->hasSyn2()) {
+      for (unsigned int i = 0; i < uniqleave.length() - 1; ++i)
+        for (unsigned int j = i + 1; j < uniqleave.length(); ++j)
+          synergy +=
+              QUACKLE_STRATEGY_PARAMETERS->syn2(uniqleave[i], uniqleave[j]);
 
-			value += synergy;
-		}    
-	}
+      // TODO handle the Q
 
-	int vowels = 0;
-	int cons = 0;
+      bool holding_bad_tile = false;
+      for (unsigned int i = 0; i < uniqleave.length(); ++i) {
+        if (QUACKLE_STRATEGY_PARAMETERS->tileWorth(uniqleave[i]) < -5.5) {
+          holding_bad_tile = true;
+        }
+      }
 
-	for (const auto& leaveIt : leave)
-	{
-		if (leaveIt != QUACKLE_BLANK_MARK)
-		{
-			if (QUACKLE_ALPHABET_PARAMETERS->isVowel(leaveIt))
-				vowels++;
-			else
-				cons++;
-		}
-	} 
+      if ((synergy > 3.0) && !holding_bad_tile) {
+        synergy += 1.5 * (synergy - 3.0);
+      }
 
-	const float vcvalues[8][8] =
-	{
-		{  0.0,   0.0,  -1.0,  -2.5,  -5.0,  -8.5, -13.5, -18.5},
-		{ -1.0,   0.0,   0.5,   0.0,  -1.5,  -5.0, -10.0,   0.0},
-		{ -3.5,  -1.0,   0.5,   1.5,  -1.5,  -3.0,   0.0,   0.0},
-		{ -7.0,  -3.5,  -0.5,   2.5,   0.0,   0.0,   0.0,   0.0},
-		{-10.0,  -6.5,  -3.0,   0.0,   0.0,   0.0,   0.0,   0.0},
-		{-13.5, -11.5,  -8.0,   0.0,   0.0,   0.0,   0.0,   0.0},
-		{-18.5, -16.5,   0.0,   0.0,   0.0,   0.0,   0.0,   0.0},
-		{-23.5,   0.0,   0.0,   0.0,   0.0,   0.0,   0.0,   0.0},
-	};
+      value += synergy;
+    }
+  }
+
+  int vowels = 0;
+  int cons = 0;
+
+  for (const auto &leaveIt : leave) {
+    if (leaveIt != QUACKLE_BLANK_MARK) {
+      if (QUACKLE_ALPHABET_PARAMETERS->isVowel(leaveIt))
+        vowels++;
+      else
+        cons++;
+    }
+  }
+
+  const float vcvalues[8][8] = {
+      {0.0, 0.0, -1.0, -2.5, -5.0, -8.5, -13.5, -18.5},
+      {-1.0, 0.0, 0.5, 0.0, -1.5, -5.0, -10.0, 0.0},
+      {-3.5, -1.0, 0.5, 1.5, -1.5, -3.0, 0.0, 0.0},
+      {-7.0, -3.5, -0.5, 2.5, 0.0, 0.0, 0.0, 0.0},
+      {-10.0, -6.5, -3.0, 0.0, 0.0, 0.0, 0.0, 0.0},
+      {-13.5, -11.5, -8.0, 0.0, 0.0, 0.0, 0.0, 0.0},
+      {-18.5, -16.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0},
+      {-23.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0},
+  };
 
 #ifdef DEBUG_BOARD
-	UVcout << QUACKLE_ALPHABET_PARAMETERS->userVisible(leave) << " has " << vowels << " vowels, " << cons << " cons.  value of " << vcvalues[vowels][cons] << endl;
+  UVcout << QUACKLE_ALPHABET_PARAMETERS->userVisible(leave) << " has " << vowels
+         << " vowels, " << cons << " cons.  value of " << vcvalues[vowels][cons]
+         << endl;
 #endif
 
-	value += vcvalues[vowels][cons];
+  value += vcvalues[vowels][cons];
 
-	if (value < -40)
-		value = -40;
+  if (value < -40)
+    value = -40;
 
 #ifdef DEBUG_BOARD
-	UVcout << "leave " << QUACKLE_ALPHABET_PARAMETERS->userVisible(leave) << " worth " << value << " uniq " << QUACKLE_ALPHABET_PARAMETERS->userVisible(uniqleave) << " synergy " << synergy << endl;
+  UVcout << "leave " << QUACKLE_ALPHABET_PARAMETERS->userVisible(leave)
+         << " worth " << value << " uniq "
+         << QUACKLE_ALPHABET_PARAMETERS->userVisible(uniqleave) << " synergy "
+         << synergy << endl;
 #endif
 
-	return value;
+  return value;
 }
-
